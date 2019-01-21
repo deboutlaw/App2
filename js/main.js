1fe1ab8dfcea6d94533a06e1abf9ac503c6bcd72
deborah outlaw <deborahaoutlaw@gmail.com>
2019-01-21 07:43:48 -0500

    uncomment listeners

--- js/main.js
+++ js/main.js
@@ -14,34 +14,34 @@ let hardBtn = document.querySelector("#hardBtn");
 
 
 
-//easyBtn.addEventListener("click", function () {
-//    hardBtn.classList.remove("selected");
-//    easyBtn.classList.add("selected");
-//    numSquares = 3;
-//    colors = generateRandomColors(numSquares);
-//    pickedColor = pickColor();
-//    colorDisplay.textContent = pickedColor;
-//    for (let i = 0; i < squares.length; i++) {
-//        if (colors[i]) {
-//            squares[i].style.backgroundColor = colors[i];
-//        } else {
-//            squares[i].style.display = "none";
-//        }
-//    }
-//});
+easyBtn.addEventListener("click", function () {
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
 
-//hardBtn.addEventListener("click", function () {
-//    easyBtn.classList.remove("selected");
-//    hardBtn.classList.add("selected");
-//    numSquares = 6;
-//    colors = generateRandomColors(numSquares);
-//    pickedColor = pickColor();
-//    colorDisplay.textContent = pickedColor;
-//    for (let i = 0; i < squares.length; i++) {
-//        squares[i].style.backgroundColor = colors[i];
-//        squares[i].style.display = "block";
-//        } 
-//});
+hardBtn.addEventListener("click", function () {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+        } 
+});
 
 for (let i = 0; i < squares.length; i++) {
     //initial color assign to squares
